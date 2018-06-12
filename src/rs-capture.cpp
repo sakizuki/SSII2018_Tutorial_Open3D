81a0911430b8d57080eadce66b14defbf0d59670
Shuichi AKIZUKI <akizuki@isl.sist.chukyo-u.ac.jp>
2018-06-13 00:26:51 +0900

    change

--- src/rs-capture.cpp
+++ src/rs-capture.cpp
@@ -79,19 +79,6 @@ int main(int argc, char * argv[])
 
     return 0;
 }
-/*
-catch (const rs2::error & e)
-{
-    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
-    return EXIT_FAILURE;
-}
-catch (const std::exception& e)
-{
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
-}
-*/
-
 
 // Convert rs2::frame to cv::Mat
 cv::Mat frame_to_mat(const rs2::frame& f)
